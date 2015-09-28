d8359c9628bab9d70dadbaee4dd95b1de3699f8c
Virtao <virtao@virtao.org>
2015-09-28 09:27:09 +0800

    Fix setGOPATH script path.

--- setGOPATH.bat
+++ setGOPATH.bat
@@ -1,4 +1,5 @@
 @echo off
 echo 修改前：GOPATH = %GOPATH%
-set GOPATH=%~dp0;%GOPATH%
+rem set GOPATH=%~dp0;%GOPATH%
+set GOPATH=%~dp0
 echo 修改后：GOPATH = %GOPATH%
--- setGOPATH.sh
+++ setGOPATH.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo 修改前：GOPATH = $GOPATH
-export GOPATH=`pwd`:$GOPATH
+# export GOPATH=`pwd`:$GOPATH
+export GOPATH=`pwd`
 echo 修改后：GOPATH = $GOPATH
